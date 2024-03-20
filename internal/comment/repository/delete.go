b4d6cd4668aa25381f37380999168d3cf5954b26
maiquocthinh <majquocthjnh@gmail.com>
2024-03-20 13:57:16 +0700

    refactor: err handle delete comic not exists

--- internal/comment/repository/delete.go
+++ internal/comment/repository/delete.go
@@ -1,11 +1,20 @@
 package repository
 
-import "context"
+import (
+	"context"
+	"errors"
+	"github.com/maiquocthinh/go-comic/pkg/common"
+)
 
 func (repo *commentRepo) DeleteCommentByID(ctx context.Context, commentID int) error {
-	_, err := repo.db.ExecContext(ctx, "DELETE FROM `comments` WHERE `id`=?", commentID)
+	result, err := repo.db.ExecContext(ctx, "DELETE FROM `comments` WHERE `id`=?", commentID)
 	if err != nil {
 		return err
 	}
+
+	if rowsAffected, _ := result.RowsAffected(); rowsAffected == 0 {
+		return common.NewBadRequestApiError(errors.New("Comment not found"), "Comment not found")
+	}
+
 	return nil
 }
